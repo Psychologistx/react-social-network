228dc9d4c7fd248c340987324d58e8e1a8b5a3c4
Psychologistx <santiberium@gmail.com>
2021-04-13 16:49:09 +0300

    Functional component to Class component

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -3,42 +3,44 @@ import React from 'react';
 import s from './Users.module.css';
 import userPhoto from '../../assets/images/male_icon.png';
 
-let Users = (props) => {
-	let getUsers = () => {
-		if (props.users.length === 0) {
-			axios.get("https://social-network.samuraijs.com/api/1.0/users")
-				.then(response => {
-					props.setUsers(response.data.items)
-				});
-		}
+class Users extends React.Component {
+
+	constructor(props) {
+		super(props);
+
+		axios.get("https://social-network.samuraijs.com/api/1.0/users")
+			.then(response => {
+				this.props.setUsers(response.data.items)
+			});
 	}
 
-	return <div>
-		<button onClick={getUsers}>Get Users</button>
-		{
-			props.users.map(u => <div key={u.id}>
-				<span>
-					<div>
-						<img src={u.photos.small !== null ? u.photos.small : userPhoto} className={s.userPhoto} alt=""/>
-					</div>
-					<div>
-						{u.followed ? <button onClick={() => { props.unfollow(u.id) }}>Unfollow</button> : <button onClick={() => { props.follow(u.id) }}>Follow</button>}
+	render() {
+		return <div>
+			{
+				this.props.users.map(u => <div key={u.id}>
+					<span>
+						<div>
+							<img src={u.photos.small !== null ? u.photos.small : userPhoto} className={s.userPhoto} alt="" />
+						</div>
+						<div>
+							{u.followed ? <button onClick={() => { this.props.unfollow(u.id) }}>Unfollow</button> : <button onClick={() => { this.props.follow(u.id) }}>Follow</button>}
 
-					</div>
-				</span>
-				<span>
-					<span>
-						<div>{u.name}</div>
-						<div>{u.status}</div>
+						</div>
 					</span>
 					<span>
-						<div>{"u.location.country"}</div>
-						<div>{"u.location.city"}</div>
+						<span>
+							<div>{u.name}</div>
+							<div>{u.status}</div>
+						</span>
+						<span>
+							<div>{"u.location.country"}</div>
+							<div>{"u.location.city"}</div>
+						</span>
 					</span>
-				</span>
-			</div>)
-		}
-	</div>
+				</div>)
+			}
+		</div>
+	}
 }
 
 export default Users;
