15caf60c5aa21aafdcfb54455b330f6ab2cb05b5
Psychologistx <santiberium@gmail.com>
2021-04-13 17:37:12 +0300

    componentDiDMount was added

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -4,10 +4,7 @@ import s from './Users.module.css';
 import userPhoto from '../../assets/images/male_icon.png';
 
 class Users extends React.Component {
-
-	constructor(props) {
-		super(props);
-
+	componentDidMount() {
 		axios.get("https://social-network.samuraijs.com/api/1.0/users")
 			.then(response => {
 				this.props.setUsers(response.data.items)
