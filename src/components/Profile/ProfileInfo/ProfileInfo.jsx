e3eba576a12d13a7525929a01a11972c42ec5828
Psychologistx <santiberium@gmail.com>
2021-04-24 19:53:56 +0300

    Showing more data in profile

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -3,10 +3,13 @@ import Preloader from '../../common/Preloader';
 import s from './ProfileInfo.module.css';
 
 let ProfileInfo = (props) => {
-	if(!props.profile) {
+
+	const jobStatus = props.profile?.lookingForAJob ? "Ищу работу" : "Работу не ищу";
+
+	if (!props.profile) {
 		return <Preloader />
 	}
-
+	console.log(props.profile)
 	return (
 		<div>
 			<div>
@@ -15,8 +18,16 @@ let ProfileInfo = (props) => {
 			<div className={s.descriptionBlock}>
 				<img className={s.avatar} src={props.profile.photos.large} alt="" />
 				{/* <img className={s.avatar} src="https://cdn.jpegmini.com/user/images/slider_puffin_before_mobile.jpg" alt="profile" /> */}
-		 + description
-		</div>
+				<section className={s.profileInformation}>
+					<div><b>Name:</b> {props.profile.fullName || "Неизвестно"}</div>
+					<div><b>About me:</b> {props.profile.aboutMe || "Неизвестно"}</div>
+					<div><b>Job status:</b> {jobStatus}</div>
+					<div><b>Contacts:</b> {Object.entries(props.profile.contacts).map(([key, value]) => {
+						return value && <div key={value}>{key} : {value}</div>
+					})}</div>
+				</section>
+
+			</div>
 		</div>
 	)
 };
